8f25b2ec765ae28c68d23120e85d4cbc55ab4591
luke7df <139607775+luke7df@users.noreply.github.com>
2023-07-17 03:17:04 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ var connectWithRetry = function () {
 
 connectWithRetry();
 
-var port = process.env.SERVER_PORT || 8080;
+var port = 8081;
 
 app.listen(port, () => {
     console.log('Servidor rodando na porta ' + port);
